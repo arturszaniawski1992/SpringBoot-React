4d8f0f93707aa14d4809f849d2b3a265d12c6e05
ASZANIAW <arturszaniawski85@gmail.com>
2019-03-13 09:05:59 +0100

    first commit for nice app

---
+++ src/test/java/com/szaniawski/artur/artur/ArturApplicationTests.java
@@ -0,0 +1,16 @@
+package com.szaniawski.artur.artur;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ArturApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
